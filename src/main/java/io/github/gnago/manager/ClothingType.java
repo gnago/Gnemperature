78eb3f7a554063d19fea34d95133078b96c7d5f7
gnago <gnagogreen@gmail.com>
2025-04-19 17:49:04 -0400

    Finished Config setup

---
+++ src/main/java/io/github/gnago/manager/ClothingType.java
@@ -0,0 +1,20 @@
+package io.github.gnago.manager;
+
+public class ClothingType {
+    public final MaterialType Material;
+    public final double Warmth;
+    public final double Resistance;
+
+    public ClothingType(MaterialType material, Double warmth, Double resistance) {
+        Material = material;
+        Warmth = warmth != null ? warmth : DefaultWarmth;
+        Resistance = resistance != null ? resistance : DefaultResistance;
+    }
+
+    public static double DefaultWarmth;
+    public static double DefaultResistance;
+
+    public enum MaterialType {
+        LEATHER, CHAINMAIL, IRON, TURTLE, GOLDEN, DIAMOND, NETHERITE
+    }
+}
--- src/main/java/io/github/gnago/manager/file/ConfigData.java
+++ src/main/java/io/github/gnago/manager/file/ConfigData.java
@@ -1,13 +1,16 @@
 package io.github.gnago.manager.file;
 
 import io.github.gnago.TemperaturePlugin;
+import io.github.gnago.manager.ClothingType;
 import org.bukkit.Material;
 import org.bukkit.Registry;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
+import java.io.File;
 import java.util.*;
 
 public class ConfigData {
@@ -51,8 +54,7 @@ public class ConfigData {
     public static HashMap<PotionEffect,Double> ResistanceEffects;
 
     public static HashMap<Enchantment,Double> ResistanceEnchantments;
-    public static HashMap<String,Double> ClothingResistances;
-    public static HashMap<String,Double> ClothingWarmth;
+    public static HashMap<ClothingType.MaterialType,ClothingType> ClothingTypes;
 
     public static int DebuffGracePeriod;
     public static HashMap<Double,List<String>> Debuffs;
@@ -63,58 +65,117 @@ public class ConfigData {
     }
 
     private final FileConfiguration configFile;
+    private final FileConfiguration materialsFile;
 
     public ConfigData() {
         this.configFile = TemperaturePlugin.getInstance().getConfig();
+        this.materialsFile = createMaterialsConfig();
 
-        RefreshRate = configFile.getInt("RefreshRate", 20);
-        EnabledWorlds = configFile.getStringList("EnabledWorlds");
-        IndoorTemperature = configFile.getDouble("IndoorTemperature", 64);
-        PeakTemperatureTime = configFile.getInt("PeakTemperatureTime", 1400);
-        EnvironmentRange = configFile.getInt("EnvironmentRange", 5);
-        InWaterModifier = configFile.getDouble("InWaterModifier", -24);
-        CloudyResistFactor = configFile.getDouble("Weather.WeakensSun", 0.6);
-        CloudyModifier = configFile.getDouble("Weather.Modifier", -10);
-        SprintingModifier = configFile.getDouble("Activity.Sprinting", 8);
-        SwimmingModifier = configFile.getDouble("Activity.Swimming", 0);
-        BurningModifier = configFile.getDouble("State.Burning", 50);
-        FreezingModifier = configFile.getDouble("State.Freezing", -50);
+        RefreshRate = configFile.getInt("refresh_rate", 20);
+        EnabledWorlds = configFile.getStringList("enabled_worlds");
+        IndoorTemperature = configFile.getDouble("indoor_temperature", 64);
+        PeakTemperatureTime = configFile.getInt("peak_temperature_time", 1400);
+        EnvironmentRange = configFile.getInt("environment_range", 5);
+        InWaterModifier = configFile.getDouble("in_water_modifier", -24);
+        CloudyResistFactor = configFile.getDouble("weather.sun_protection", 0.6);
+        CloudyModifier = configFile.getDouble("weather.modifier", -10);
+        SprintingModifier = configFile.getDouble("activity.sprinting", 8);
+        SwimmingModifier = configFile.getDouble("activity.swimming", 0);
+        BurningModifier = configFile.getDouble("state.burning", 50);
+        FreezingModifier = configFile.getDouble("state.freezing", -50);
 
         GradualityRates = new HashMap<>();
-        GradualityRates.put(TemperatureType.SUNLIGHT, configFile.getDouble("Graduality.Climate",0.12));
-        GradualityRates.put(TemperatureType.ENVIRONMENT, configFile.getDouble("Graduality.Environment",0.15));
-        GradualityRates.put(TemperatureType.WATER, configFile.getDouble("Graduality.Water",0.75));
-        GradualityRates.put(TemperatureType.CLOTHING, configFile.getDouble("Graduality.Clothing",0.25));
-        GradualityRates.put(TemperatureType.TOOL, configFile.getDouble("Graduality.Tool",0.7));
-        GradualityRates.put(TemperatureType.ACTIVITY, configFile.getDouble("Graduality.Activity",0.02));
-        GradualityRates.put(TemperatureType.STATE, configFile.getDouble("Graduality.State",0.8));
+        GradualityRates.put(TemperatureType.SUNLIGHT, configFile.getDouble("graduality.climate",0.12));
+        GradualityRates.put(TemperatureType.ENVIRONMENT, configFile.getDouble("graduality.environment",0.15));
+        GradualityRates.put(TemperatureType.WATER, configFile.getDouble("graduality.water",0.75));
+        GradualityRates.put(TemperatureType.CLOTHING, configFile.getDouble("graduality.clothing",0.25));
+        GradualityRates.put(TemperatureType.TOOL, configFile.getDouble("graduality.tool",0.7));
+        GradualityRates.put(TemperatureType.ACTIVITY, configFile.getDouble("graduality.activity",0.02));
+        GradualityRates.put(TemperatureType.STATE, configFile.getDouble("graduality.state",0.8));
 
-        WetnessModifier = configFile.getDouble("Wetness.Modifier", -16);
-        WetnessMax = configFile.getDouble("Wetness.Max", 60);
-        WetnessIncrement = configFile.getDouble("Wetness.Increment", 5);
-        WetnessDecrement = configFile.getDouble("Wetness.Decrement", 1);
-        UseWetnessDegradation = configFile.getBoolean("Wetness.EnableDegradation", true);
+        WetnessModifier = configFile.getDouble("wetness.modifier", -16);
+        WetnessMax = configFile.getDouble("wetness.max", 60);
+        WetnessIncrement = configFile.getDouble("wetness.increment", 5);
+        WetnessDecrement = configFile.getDouble("wetness.decrement", 1);
+        UseWetnessDegradation = configFile.getBoolean("wetness.enable_degradation", true);
 
-        IdealTemperature = configFile.getDouble("Resistance.Target", 75);
-        ResistTemperatureMax = configFile.getDouble("Resistance.PotionEffects.MaxTemperature", 90);
-        ResistTemperatureMin = configFile.getDouble("Resistance.PotionEffects.MinTemperature", 10);
+        IdealTemperature = configFile.getDouble("resistance.ideal", 75);
+        ResistTemperatureMax = configFile.getDouble("resistance.potion_effects.max_temperature", 90);
+        ResistTemperatureMin = configFile.getDouble("resistance.potion_effects.min_temperature", 10);
 
-        HungerMidPoint = configFile.getDouble("Resistance.Hunger.Mid-point", 3.5);
-        HungerMaxResist = configFile.getDouble("Resistance.Hunger.Resistance", 0.15);
-        HungerMaxVuln = configFile.getDouble("Resistance.Hunger.Vulnerability", 0.4);
-        ThirstMidPoint = configFile.getDouble("Resistance.Thirst.Mid-point", 40);
-        ThirstMaxResist = configFile.getDouble("Resistance.Thirst.Resistance", 0.15);
-        ThirstMaxVuln = configFile.getDouble("Resistance.Thirst.Vulnerability", 0.4);
+        HungerMidPoint = configFile.getDouble("resistance.hunger.midpoint", 3.5);
+        HungerMaxResist = configFile.getDouble("resistance.hunger.resistance", 0.15);
+        HungerMaxVuln = configFile.getDouble("resistance.hunger.vulnerability", 0.4);
+        ThirstMidPoint = configFile.getDouble("resistance.thirst.midpoint", 40);
+        ThirstMaxResist = configFile.getDouble("resistance.thirst.resistance", 0.15);
+        ThirstMaxVuln = configFile.getDouble("resistance.thirst.vulnerability", 0.4);
 
-        DebuffGracePeriod = configFile.getInt("Debuffs.GracePeriod", 30);
+        DebuffGracePeriod = configFile.getInt("debuffs.grace_period", 30);
         Debuffs = new HashMap<>();
-        configFile.getConfigurationSection("Debuffs").getKeys(false).forEach(
+        configFile.getConfigurationSection("debuffs").getKeys(false).forEach(
                 key -> {
-                    Collection<String> effects = configFile.getConfigurationSection("Debuffs."+key).getKeys(false);
+                    Collection<String> effects = configFile.getConfigurationSection("debuffs."+key).getKeys(false);
                     Debuffs.put(Double.parseDouble(key), new ArrayList<>(effects));
                 });
 
-        //todo add clothing stuff
-        //then put the item/block temperatures in a different config file, since that will be pretty big
+        ClothingTypes = new HashMap<>();
+        ClothingType.DefaultWarmth = configFile.getDouble("clothing.default.warmth");
+        ClothingType.DefaultResistance = configFile.getDouble("clothing.default.resistance");
+        for (ClothingType.MaterialType mat : ClothingType.MaterialType.values()) {
+            String matName = mat.name().toLowerCase();
+            ClothingTypes.put(mat, new ClothingType(mat,
+                    configFile.getDouble("clothing." + matName + ".warmth"),
+                    configFile.getDouble("clothing." + matName + ".resistance")));
+        }
+
+        configFile.getConfigurationSection("resistance.potion_effects.list").getKeys(false).forEach(
+                key -> {
+                    PotionEffectType effType = Registry.EFFECT.match(key.toUpperCase());
+                    if (effType != null)
+                        ResistanceEffects.put(
+                                new PotionEffect(effType, 8, 0),
+                                configFile.getDouble("resistance.potion_effects.list." + key, 0)
+                        );
+                });
+        configFile.getConfigurationSection("resistance.enchantments.list").getKeys(false).forEach(
+                key -> {
+                    Enchantment enchant = Registry.ENCHANTMENT.match(key.toUpperCase());
+                    if (enchant != null)
+                        ResistanceEnchantments.put(
+                                enchant,
+                                configFile.getDouble("resistance.enchantments.list." + key, 0)
+                        );
+                });
+
+        materialsFile.getConfigurationSection("blocks").getKeys(false).forEach(
+                key -> {
+                    Material mat = Registry.MATERIAL.match(key.toUpperCase());
+                    if (mat != null)
+                        BlockTemperatures.put(
+                                mat,
+                                materialsFile.getDouble("blocks." + key, 0)
+                        );
+                }
+        );
+        materialsFile.getConfigurationSection("items").getKeys(false).forEach(
+                key -> {
+                    Material mat = Registry.MATERIAL.match(key.toUpperCase());
+                    if (mat != null)
+                        ItemTemperatures.put(
+                                mat,
+                                materialsFile.getDouble("items." + key, 0)
+                        );
+                }
+        );
+    }
+
+    private static FileConfiguration createMaterialsConfig() {
+        File file = new File(TemperaturePlugin.getInstance().getDataFolder(), "materials.yml");
+        if (!file.exists()) {
+            file.getParentFile().mkdirs();
+            TemperaturePlugin.getInstance().saveResource("materials.yml", false);
+        }
+
+        return YamlConfiguration.loadConfiguration(file);
     }
 }
